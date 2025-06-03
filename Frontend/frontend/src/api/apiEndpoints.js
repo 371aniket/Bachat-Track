1bd40364ce06398625c17e59b5b06a16d5607c5c
371aniket <aniketkumar882858@gmail.com>
2025-06-03 18:11:52 +0530

    Describe your new changes here

--- Frontend/frontend/src/api/apiEndpoints.js
+++ Frontend/frontend/src/api/apiEndpoints.js
@@ -1,5 +1,5 @@
 // Define your base URL here
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://bachat-track-backend.onrender.com/api/v1";
 
 // Auth endpoints
 export const AUTH_API = {
