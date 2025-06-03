d0899ff4e2507113d4f915894dd33a64baefd69d
371aniket <aniketkumar882858@gmail.com>
2025-06-03 17:47:52 +0530

    Describe your new changes here

--- Backend/controllers/income.js
+++ Backend/controllers/income.js
@@ -1,65 +1,49 @@
-const IncomeSchema= require("../models/IncomeModel")
-
+const Income = require("../models/incomeModel");  // or "../models/IncomeModel" based on actual filename
 
 exports.addIncome = async (req, res) => {
-    const {title, amount, category, description, date}  = req.body
+    const {title, amount, category, description, date}  = req.body;
 
-
-    // below is the instance of the income schema created
-    const income = IncomeSchema({
+    const income = new Income({
         title,
         amount,
         category,
         description,
         date
-    })
-
-     // to save this data into the database
+    });
 
     try {
-//         //validations
-        if(!title || !category || !description || !date||!amount){
-            return res.status(400).json({message: 'All fields are required!'})
+        if(!title || !category || !description || !date || !amount){
+            return res.status(400).json({message: 'All fields are required!'});
         }
-        if(amount <= 0 || !amount === 'number'){
-            return res.status(400).json({message: 'Amount must be a positive number!'})
+        if(amount <= 0 || typeof amount !== 'number'){
+            return res.status(400).json({message: 'Amount must be a positive number!'});
         }
 
-        // saving the instance income to the database
-        await income.save()
-        res.status(200).json({message: 'Income Added'})
+        await income.save();
+        res.status(200).json({message: 'Income Added'});
     } 
     catch (error) {
-        res.status(500).json({message: 'Server Error'})
+        res.status(500).json({message: 'Server Error'});
     }
-    console.log(income)
-}
- // method for getting all of our documents
+    console.log(income);
+};
 
 exports.getIncomes = async (req, res) =>{
     try {
-        // we are writing the below line and also sorting it because we want to keep our last created item at the top rather than at the bottom in the list , hence sort
-        const incomes = await IncomeSchema.find().sort({createdAt: -1})
-        res.status(200).json(incomes)
+        const incomes = await Income.find().sort({createdAt: -1});
+        res.status(200).json(incomes);
     } catch (error) {
-        res.status(500).json({message: 'Server Error'})
+        res.status(500).json({message: 'Server Error'});
     }
-}
-
-
-// creating a delete function
+};
 
 exports.deleteIncome = async (req, res) =>{
     const {id} = req.params;
-    IncomeSchema.findByIdAndDelete(id)
-
-       // callback function
-        .then((income) =>{
-            res.status(200).json({message: 'Income Deleted'})
+    Income.findByIdAndDelete(id)
+        .then(() => {
+            res.status(200).json({message: 'Income Deleted'});
         })
-        .catch((err) =>{
-            res.status(500).json({message: 'Server Error'})
-        })
-
-  
-    }
+        .catch(() => {
+            res.status(500).json({message: 'Server Error'});
+        });
+};
